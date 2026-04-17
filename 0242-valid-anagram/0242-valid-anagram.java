a05a8a0659c2c20fbad7bb6ffbba2e92fa714b53
Atul Pandey <atul.23scse1410273@galgotiasuniversity.ac.in>
2026-04-17 11:06:06 +0530

    Time: 3 ms (92.65%), Space: 46.3 MB (41.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) return false;
+//         int[] count=new int[26];
+//         for(int i=0;i<s.length();i++){
+//            count[s.charAt(i)-'a']++;
+//            count[t.charAt(i)-'a']--;
+//         }
+//         for(int val:count){
+//             if(val!=0){
+//                 return false;}
+//             }
+//         return true;
+//     }
+// }
+
+
+
+class Solution{
+    public boolean isAnagram(String s ,String t){
+        if(s.length()!=t.length()) return false;
+        char[] s1=s.toCharArray();
+        char[] t1=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        
+        return Arrays.equals(s1,t1);
+    }
+}
