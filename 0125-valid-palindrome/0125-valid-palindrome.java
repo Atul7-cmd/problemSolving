28c0fff296d9eceaaab372338da202f9dba27e61
Atul Pandey <atul.23scse1410273@galgotiasuniversity.ac.in>
2026-04-18 15:26:46 +0530

    Time: 4 ms (48.37%), Space: 44.8 MB (44.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//       StringBuilder sb=new StringBuilder();
+//       int n=s.length();
+//       for(int i=0;i<n;i++){
+//         char ch=s.charAt(i);
+//         if(Character.isUpperCase(ch)){
+//             char c=Character.toLowerCase(ch);
+//             sb.append(c);
+//         }
+//         if((ch>='0' && ch<='9')||(ch>='a'&&ch<='z')){
+//             sb.append(ch);
+//         }
+//       }  
+//       String ori=sb.toString();
+//       String rev=sb.reverse().toString();
+//      return ori.equals(rev);
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-      StringBuilder sb=new StringBuilder();
-      int n=s.length();
-      for(int i=0;i<n;i++){
-        char ch=s.charAt(i);
-        if(Character.isUpperCase(ch)){
-            char c=Character.toLowerCase(ch);
-            sb.append(c);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            }
         }
-        if((ch>='0' && ch<='9')||(ch>='a'&&ch<='z')){
-            sb.append(ch);
-        }
-      }  
-      String ori=sb.toString();
-      String rev=sb.reverse().toString();
-     return ori.equals(rev);
+
+        String ori = sb.toString();
+        String rev = new StringBuilder(ori).reverse().toString();
+
+        return ori.equals(rev);
     }
 }
