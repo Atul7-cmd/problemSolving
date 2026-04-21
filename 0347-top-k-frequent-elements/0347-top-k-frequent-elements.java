98083727d1f0f586f9ed919afcde6cd8630b65e7
Atul Pandey <atul.23scse1410273@galgotiasuniversity.ac.in>
2026-04-22 00:17:16 +0530

    Time: 12 ms (84.72%), Space: 47.5 MB (82.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,65 @@
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         int n = nums.length;
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(
+//             (a, b) -> map.get(a) - map.get(b));
+
+//         for (int i = 0; i < n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+
+//         int[] res = new int[k];
+
+//         for (int key : map.keySet()) {
+//             pq.add(key);   
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+
+//         int i = 0;
+
+//         while (!pq.isEmpty()) {
+//             res[i++] = pq.poll();  
+//         }
+
+//         return res;
+//     }
+// }
+
+//  Bucket Sort
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
+        // Step 1: Frequency map
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a, b) -> map.get(a) - map.get(b));
+        // Step 2: Bucket array
+        List<Integer>[] bucket = new List[nums.length + 1];
 
-       
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int key : freq.keySet()) {
+            int f = freq.get(key);
+            if (bucket[f] == null) {
+                bucket[f] = new ArrayList<>();
+            }
+            bucket[f].add(key);
         }
 
         int[] res = new int[k];
+        int idx = 0;
 
-        
-        for (int key : map.keySet()) {
-            pq.add(key);   
-            if (pq.size() > k) {
-                pq.poll();
+        for (int i = bucket.length - 1; i >= 0 && idx < k; i--) {
+            if (bucket[i] != null) {
+                for (int num : bucket[i]) {
+                    res[idx++] = num;
+                    if (idx == k)
+                        break;
+                }
             }
-        }
-
-        int i = 0;
-
-
-        while (!pq.isEmpty()) {
-            res[i++] = pq.poll();  
         }
 
         return res;
